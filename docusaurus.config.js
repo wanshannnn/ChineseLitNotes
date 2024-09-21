c4afdd74d154ccad26643d8a2b08379ab5b1e978
wanshannnn <3179157197@qq.com>
2024-09-21 22:02:23 +0800

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,90 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ChineseLitNotes',
+  tagline: '一个中文系相关的网站',
+
+  url: 'https://wanshannnn.github.io',
+  baseUrl: '/',
+
+  organizationName: 'wanshannnn',
+  projectName: 'ChineseLitNotes',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'ChineseLitNotes',
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'Sidebar1',
+            position: 'left',
+            label: '现代文学',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'Sidebar2',
+            position: 'left',
+            label: '当代文学',
+          },
+          {
+            href: 'https://github.com/wanshannnn/ChineseLitNotes',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} ChineseLitNotes, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
