9a050eca9ddb6346f97c0fe9613fe24c9a719a0f
wanshannnn <3179157197@qq.com>
2024-10-10 21:01:50 +0800

    docs: 修改底部copyright内容

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -5,7 +5,7 @@ const CustomFooter = () => {
   return (
     <footer style={{ backgroundColor: '#333', padding: '1rem', color: '#fff', textAlign: 'center' }}>
       <p style={{ margin: 0 }}>
-        Copyright © {new Date().getFullYear()} wanshannnn. All Rights Reserved.
+        Copyright © {new Date().getFullYear()} ChineseLitNotes. All Rights Reserved.
       </p>
       <div id="footer-view-count">
         {typeof window !== 'undefined' && (
